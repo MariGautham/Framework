276d26a419ac679e61759379c1572b000affdaca
MariGautham <mari15390@gmail.com>
2024-11-24 11:05:43 +0530

    Updated

--- src/test/java/org/baseclass/BaseClass.java
+++ src/test/java/org/baseclass/BaseClass.java
@@ -1,6 +1,15 @@
 package org.baseclass;
 
+import io.opentelemetry.exporter.logging.SystemOutLogRecordExporter;
+
 public class BaseClass {
+	public void trichy() {
+		System.out.println("Trichy");
+
+	}
+	public void chennai() {
+		System.out.println("Chennai");
+	}
 	
 
 }
